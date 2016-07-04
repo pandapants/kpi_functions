e43f7a3a744b2514bed96e4aba7c632d45c88321
“Amanda” <amanda.tjernlund@gmail.com>
2016-07-04 12:18:12 +0100

    basic userData tests passing

--- KPIFunctions.js
+++ KPIFunctions.js
@@ -1,1 +1,57 @@
+var moment = require('moment');
+
+var format = {};
+
+format.getStartDate = (reportingPeriods) => {
+  const startDate = reportingPeriods[0]['begin'];
+
+  return startDate;
+};
+
+format.getEndDate = (reportingPeriods) => {
+  const endDate = reportingPeriods[reportingPeriods.length - 1]['end'];
+
+  return endDate;
+};
+
+format.getReportingYearLength = (reportingPeriods) => {
+  const startDate = format.getStartDate(reportingPeriods);
+  const endDate = format.getEndDate(reportingPeriods);
+
+  const from = moment(startDate, 'YYYY-MM-DD');
+  const to = moment(endDate, 'YYYY-MM-DD');
+  const reportingYear = to.diff(from, 'days');
+
+  return reportingYear + 1;
+};
+
+format.getDaysIntoReportingYear = (reportingPeriods, currentDate) => {
+  const startDate = format.getStartDate(reportingPeriods);
+
+  const from = moment(startDate.toString(), 'YYYY-MM-DD');
+  const to = moment(currentDate, 'YYYY-MM-DD');
+  const daysIntoYear = to.diff(from, 'days');
+
+  return daysIntoYear + 1;
+};
+
+format.getYTDTarget = (totalTarget, reportingYearLength, daysIntoYear) => {
+  var YTDTarget = (daysIntoYear / reportingYearLength) * totalTarget;
+  return Math.round(YTDTarget);
+};
+
+format.getYTDPercentage = (totalTarget, YTDTarget) => {
+  var YTDPercentage =  ((YTDTarget / totalTarget) * 100).toFixed(1);
+  return +YTDPercentage;
+};
+
+format.getTotalAchieved = (metric, networkData, startDate, todaysDate) => {
+// this.props.state.networkData.[network].[metric].dailyData
+// each metric currently contains a dailyDataArray of 600 days
+
+//based on start date and today's date get relevant data under networkData....
+};
+
+
+module.exports = format;
 
---
+++ spec/kpifunctions.js
@@ -0,0 +1,112 @@
+/* global it, describe, afterEach */
+
+var expect = require('chai').expect;
+var format = require('../kpifunctions.js');
+
+
+// networkData has daily data for period 2016-01-23 - 2016-06-20
+
+// want to get:
+// totalAchieved: 4432,
+// totalPercent: 17,
+// YTDTarget: 18000,
+// YTDPercent: 30
+
+describe('format', function () {
+  var reportingPeriods = [
+    {
+      begin: '2016-06-01',
+      end: '2016-06-30'
+    },
+    {
+      begin: '2016-07-01',
+      end:'2016-07-31'
+    },
+    {
+      begin: '2016-08-01',
+      end: '2016-08-31'
+    }
+  ];
+  describe('getStartDate', function () {
+    var result = format.getStartDate(reportingPeriods);
+
+    it('is a function', function () {
+      expect(format.getStartDate).to.be.a.function;
+    });
+    it('returns a string', function () {
+      expect(result).to.be.a.string;
+    });
+    it('returns the correct start date for reporting year', function () {
+      expect(result).to.eql('2016-06-01');
+    });
+  });
+  describe('getEndDate', function () {
+    var result = format.getEndDate(reportingPeriods);
+    it('is a function', function () {
+      expect(format.getEndDate).to.be.a.function;
+    });
+    it('returns a string', function () {
+      expect(result).to.be.a.string;
+    });
+    it('returns the correct end date for reporting year', function () {
+      expect(result).to.eql('2016-08-31');
+    });
+  });
+  describe('getReportingYearLength', function () {
+    var result = format.getReportingYearLength(reportingPeriods);
+    it('is a function', function () {
+      expect(format.getReportingYearLength).to.be.a.function;
+    });
+    it('returns a number', function () {
+      expect(result).to.be.a.number;
+    });
+    it('returns the correct number of days', function () {
+      expect(result).to.eql(92);
+    });
+  });
+  describe('getDaysIntoReportingYear', function () {
+    var result = format.getDaysIntoReportingYear(reportingPeriods, '2016-06-29');
+    it('is a function', function () {
+      expect(format.getDaysIntoReportingYear).to.be.a.function;
+    });
+    it('returns a number', function () {
+      expect(result).to.be.a.number;
+    });
+    it('returns the correct number of days', function () {
+      expect(result).to.eql(29);
+    });
+  });
+  describe('getYTDTarget', function () {
+    var totalTarget = 5000;
+    var reportingYear = 365;
+    var daysIntoYear = 100;
+
+    var result = format.getYTDTarget(totalTarget, reportingYear, daysIntoYear);
+    it('is a function', function () {
+      expect(format.getYTDTarget).to.be.a.function;
+    });
+    it('returns a number', function () {
+      expect(result).to.be.a.number;
+    });
+    it('returns the correct YTD target', function () {
+      expect(result).to.eql(1370);
+    });
+  });
+  describe('getYTDPercentage', function () {
+    var totalTarget = 5000;
+    var YTDTarget = 1370;
+    var result = format.getYTDPercentage(totalTarget, YTDTarget);
+
+    it('is a function', function () {
+      expect(format.getYTDPercentage).to.be.a.function;
+    });
+    it('returns a number', function () {
+      expect(result).to.be.a.number;
+    });
+    it('returns the correct percentage, rounded to 1 decimal', function () {
+      expect(result).to.eql(27.4);
+      expect(format.getYTDPercentage(4600, 300)).to.eql(6.5);
+      expect(format.getYTDPercentage(3250, 2000)).to.eql(61.5);
+    });
+  });
+});
